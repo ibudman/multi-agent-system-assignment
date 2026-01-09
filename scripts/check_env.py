2e884322326a10abde4cfedc24f818ed526836bb
Inbar Budman <inbarb123@gmail.com>
2026-01-09 01:23:49 -0500

    cleanup scripts

--- scripts/check_env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-required_vars = ["OPENAI_API_KEY", "TAVILY_API_KEY", "MONGODB_URI"]
-
-for var in required_vars:
-    value = os.getenv(var)
-    print(f"{var} loaded? {bool(value)}")
